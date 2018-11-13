5b88accb9c37a72b0b4742b5deaba44d169e0ba9
Yunxi Xiong <xiong@Yunxis-MacBook-Pro.local>
2018-11-13 01:37:13 -0600

    finished the front-end for authentication using Amazon Cognito to login and sign up. Take a session-based approach for the authtication

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component, Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
+import Routes from "./Routes";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isAuthenticated: false,
+            isAuthenticating: true
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            await Auth.currentSession();
+            this.userHasAuthenticated(true);
+        }
+        catch(e) {
+            // none
+        }
+
+        this.setState({ isAuthenticating: false });
+    }
+
+    userHasAuthenticated = authenticated => {
+        this.setState({ isAuthenticated: authenticated });
+    }
+
+    handleLogout = async event => {
+        await Auth.signOut();
+
+        this.userHasAuthenticated(false);
+        this.props.history.push("/login");
+    }
+
+    render() {
+        const childProps = {
+            isAuthenticated: this.state.isAuthenticated,
+            userHasAuthenticated: this.userHasAuthenticated
+        };
+
+        return (
+            !this.state.isAuthenticating &&
+            <div className="App container">
+            <Navbar fluid inverse collapseOnSelect style={{ padding: '10px'}}>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">
+                            Mcgugin Dining Center Menu
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        {this.state.isAuthenticated
+                            ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                            : <Fragment>
+                                <LinkContainer to="/signup">
+                                    <NavItem>Signup</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/login">
+                                    <NavItem>Login</NavItem>
+                                </LinkContainer>
+                            </Fragment>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes childProps={childProps}/>
+            </div>
+        );
+    }
+}
+
+export default withRouter(App);
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>Mcgugin Dining Center Menu</h1>
+                    <p>Sign in to see the weekly menu</p>
+                </div>
+            </div>
+        );
+    }
+}

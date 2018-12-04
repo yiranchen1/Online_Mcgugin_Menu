25e70c23e37791a897f1cecae8e14be9d99912ec
Yunxi Xiong <xiong@Yunxis-MacBook-Pro.local>
2018-12-04 01:18:00 -0600

    add test for the app componenet

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mount } from 'enzyme';
+import App from './App'
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { Link } from 'react-router-dom';
+import { StaticRouter } from 'react-router'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+    describe('when is Authenticated', () => {
+    it('should render menu button', () => {
+        const wrapper = mount(
+            <App isAutenticated={false}>
+        <div>ahoy!</div>
+    </App>
+);
+    expect(wrapper.html()).toEqual('<div>ahoy!</div>');
+    wrapper.unmount();
 });
+});
+});
+
+const logout = require(App)
+test(`async logout passed'`, async () => {
+    const str = await logout()
+    expect(str).toBe('TEST')
+})
+
+describe('Test the main page', () => {
+    it('renders correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+});
+});
+
+test('Link to login', () => {
+    const component = renderer.create(
+        <StaticRouter>
+        <Link to="/login" />
+    </StaticRouter>
+);
+
+let tree = component.toJSON();
+expect(tree).toMatchSnapshot();
+});
+
+test('Link to signup', () => {
+    const component = renderer.create(
+        <StaticRouter>
+        <Link to="/signup" />
+    </StaticRouter>
+);
+
+let tree = component.toJSON();
+expect(tree).toMatchSnapshot();
+});
+

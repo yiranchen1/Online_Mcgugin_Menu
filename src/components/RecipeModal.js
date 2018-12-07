199e488e188b38797b6ff4116b434870afbfbd44
Manyao Peng <>
2018-12-07 17:34:55 -0600

    recipemodal test

--- src/components/RecipeModal.js
+++ src/components/RecipeModal.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import '../App.css';
 import './RecipeModal.css';
 import Modal from '@material-ui/core/Modal';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import $ from  'jquery'
 
